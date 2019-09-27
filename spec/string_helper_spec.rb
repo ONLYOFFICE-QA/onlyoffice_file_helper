bcfb80665823a312660a4c276a6980f9c32bf072
Pavel Lobashov <shockwavenn@gmail.com>
2019-09-27 15:35:24 +0300

    Fix more

--- spec/string_helper_spec.rb
+++ spec/string_helper_spec.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe OnlyofficeFileHelper::StringHelper do
+  it 'StringHelper.generate_random_string' do
+    expect(OnlyofficeFileHelper::StringHelper.generate_random_string(50).length).to eq(50)
+  end
+
+  it 'StringHelper.generate_random_number' do
+    expect(OnlyofficeFileHelper::StringHelper.generate_random_number('Test')).not_to be_empty
+  end
+
+  it 'StringHelper.get_result_string_of_comparison' do
+    expect(OnlyofficeFileHelper::StringHelper.get_result_string_of_comparison('Test',
+                                                                              'Data', 'Data')).to eq('')
+  end
+
   it 'StringHelper.to_bool check true' do
     expect(OnlyofficeFileHelper::StringHelper.to_bool('true')).to be_truthy
   end
