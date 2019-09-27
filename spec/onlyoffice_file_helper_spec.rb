4fe473e141a339a2908dd0829d74d33331a5574e
Pavel Lobashov <shockwavenn@gmail.com>
2019-09-27 17:21:47 +0300

    Add more tests

--- spec/onlyoffice_file_helper_spec.rb
+++ spec/onlyoffice_file_helper_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe OnlyofficeFileHelper do
@@ -54,5 +56,26 @@ RSpec.describe OnlyofficeFileHelper do
     it 'list_file_in_directory empty for non-existing folder' do
       expect(OnlyofficeFileHelper::FileHelper.list_file_in_directory('/incorrect-path')).to be_empty
     end
+  end
+
+  it 'get_filename' do
+    expect(OnlyofficeFileHelper::FileHelper.get_filename('/usr/share/fonts')).to eq('fonts')
+  end
+
+  it 'create_folder' do
+    expect(OnlyofficeFileHelper::FileHelper.create_folder('/tmp/tmp')).not_to be_nil
+  end
+
+  it 'delete_directory' do
+    OnlyofficeFileHelper::FileHelper.create_folder('/tmp/tmp')
+    expect(OnlyofficeFileHelper::FileHelper.delete_directory('/tmp/tmp')).not_to be_nil
+  end
+
+  it 'directory_hash' do
+    expect(OnlyofficeFileHelper::FileHelper.directory_hash('.')).not_to be_nil
+  end
+
+  it 'create_file_with_size' do
+    expect(OnlyofficeFileHelper::FileHelper.create_file_with_size(size: '1K')).not_to be_nil
   end
 end
