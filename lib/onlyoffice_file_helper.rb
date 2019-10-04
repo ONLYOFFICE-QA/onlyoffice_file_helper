02de0a955e03bb6bc8ac90e2812f461c596aa297
Pavel Lobashov <shockwavenn@gmail.com>
2019-10-04 13:28:35 +0300

    Fix RuboCop `Style/FrozenStringLiteralComment` cop

--- lib/onlyoffice_file_helper.rb
+++ lib/onlyoffice_file_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'zip'
 require 'open-uri'
--- lib/onlyoffice_file_helper/string_helper.rb
+++ lib/onlyoffice_file_helper/string_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module OnlyofficeFileHelper
   # Helper for Strings
   class StringHelper
