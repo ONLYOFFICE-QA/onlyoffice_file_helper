f3d5170809a1581628a96e9d54730b9817e1bebc
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-06 11:37:29 +0300

    Reduce some metrics

--- spec/onlyoffice_file_helper/read_file_to_string_spec.rb
+++ spec/onlyoffice_file_helper/read_file_to_string_spec.rb
@@ -5,19 +5,16 @@ require 'spec_helper'
 RSpec.describe OnlyofficeFileHelper, '.read_file_to_string' do
   it 'create_file_with_content without parameters' do
     file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content
-    expect(File).to exist(file_path)
     expect(OnlyofficeFileHelper::FileHelper.read_file_to_string(file_path)).to be_empty
   end
 
   it 'create_file_with_content with custom name' do
     file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: '/tmp/custom.file.name')
-    expect(File).to exist(file_path)
     expect(OnlyofficeFileHelper::FileHelper.read_file_to_string(file_path)).to be_empty
   end
 
   it 'create_file_with_content with custom content and name' do
     file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: '/tmp/custom2.file.name', content: 'test_sting')
-    expect(File).to exist(file_path)
     expect(OnlyofficeFileHelper::FileHelper.read_file_to_string(file_path)).to eq('test_sting')
   end
 
