3aecc41323b7c2270f62cd9933b5d5b717b84043
Pavel Lobashov <shockwavenn@gmail.com>
2020-04-16 11:09:39 +0300

    Fix rubocop `RSpec/MultipleExpectations` cop

--- spec/onlyoffice_file_helper/file_helper_create_file_with_content_spec.rb
+++ spec/onlyoffice_file_helper/file_helper_create_file_with_content_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.describe OnlyofficeFileHelper::FileHelper do
+RSpec.describe OnlyofficeFileHelper::FileHelper, '.create_file_with_content' do
   it 'file_line_count zero' do
     file_path = described_class.create_file_with_content
     expect(described_class.file_line_count(file_path)).to eq(0)
@@ -13,9 +13,15 @@ RSpec.describe OnlyofficeFileHelper::FileHelper do
     expect(described_class.file_line_count(file_path)).to eq(2)
   end
 
-  it 'file_line' do
-    file_path = described_class.create_file_with_content(content: "a\nb\n")
-    expect(described_class.read_specific_line(file_path, 0)).to eq('a')
-    expect(described_class.read_specific_line(file_path, 1)).to eq('b')
+  describe 'correct lines' do
+    let(:file_path) { described_class.create_file_with_content(content: "a\nb\n") }
+
+    it 'file_line first correct' do
+      expect(described_class.read_specific_line(file_path, 0)).to eq('a')
+    end
+
+    it 'file_line second correct' do
+      expect(described_class.read_specific_line(file_path, 1)).to eq('b')
+    end
   end
 end
