2e006e972f78093e002deb9d47a313988a1b9932
Pavel Lobashov <shockwavenn@gmail.com>
2020-04-16 11:25:53 +0300

    Reduce metrics length

---
+++ spec/onlyoffice_file_helper/file_helper_read_file_to_string_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe OnlyofficeFileHelper::FileHelper,
+               '.read_file_to_string' do
+  it 'create_file_with_content without parameters' do
+    file_path = described_class.create_file_with_content
+    expect(described_class.read_file_to_string(file_path)).to be_empty
+  end
+
+  it 'create_file_with_content with custom name' do
+    file_path = described_class.create_file_with_content(file_path: '/tmp/custom.file.name')
+    expect(described_class.read_file_to_string(file_path)).to be_empty
+  end
+
+  it 'create_file_with_content with custom content and name' do
+    file_path = described_class.create_file_with_content(file_path: '/tmp/custom2.file.name',
+                                                         content: 'test_sting')
+    expect(described_class.read_file_to_string(file_path)).to eq('test_sting')
+  end
+
+  it 'create_file_with_content with custom string with escaping symbols' do
+    file_path = described_class.create_file_with_content(file_path: '/tmp/custom2.file.name',
+                                                         content: iframe_html_string)
+    expect(described_class.read_file_to_string(file_path)).to eq(string)
+  end
+end
