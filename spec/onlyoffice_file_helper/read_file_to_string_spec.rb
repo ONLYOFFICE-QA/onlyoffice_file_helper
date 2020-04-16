9c622944ef8eeac32af2f85f5404cc0ab312963a
Pavel Lobashov <shockwavenn@gmail.com>
2020-04-16 11:24:34 +0300

    Reduce metrics

--- spec/onlyoffice_file_helper/read_file_to_string_spec.rb
+++ spec/onlyoffice_file_helper/read_file_to_string_spec.rb
@@ -14,18 +14,14 @@ RSpec.describe OnlyofficeFileHelper, '.read_file_to_string' do
   end
 
   it 'create_file_with_content with custom content and name' do
-    file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: '/tmp/custom2.file.name', content: 'test_sting')
+    file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: '/tmp/custom2.file.name',
+                                                                          content: 'test_sting')
     expect(OnlyofficeFileHelper::FileHelper.read_file_to_string(file_path)).to eq('test_sting')
   end
 
   it 'create_file_with_content with custom string with escaping symbols' do
-    string = '<iframe src="https://doc-linux-autotest.teamlab.info'\
-             '/products/files/doceditor.aspx?fileid=2019451&'\
-             'doc=cW5kWFBDUFlRazBRcE5CekJBU3NsVDZhUUcyY'\
-             'S9oMHlhaGQrd3BSVU5qND0_IjIwMTk0NTEi0'\
-             '&action=embedded" height="100%" width="100%" '\
-             'frameborder="0" scrolling="no" allowtransparency></iframe>'
-    file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: '/tmp/custom2.file.name', content: string)
+    file_path = OnlyofficeFileHelper::FileHelper.create_file_with_content(file_path: '/tmp/custom2.file.name',
+                                                                          content: iframe_html_string)
     expect(OnlyofficeFileHelper::FileHelper.read_file_to_string(file_path)).to eq(string)
   end
 end
