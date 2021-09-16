118b76f4625e6f4ed33b1c59ca82e0f23b7a0d9d
Pavel Lobashov <shockwavenn@gmail.com>
2021-09-16 14:46:26 +0300

    Remove duplicate

--- spec/onlyoffice_file_helper/file_helper_spec.rb
+++ spec/onlyoffice_file_helper/file_helper_spec.rb
@@ -23,9 +23,4 @@ RSpec.describe OnlyofficeFileHelper::FileHelper do
   it 'output_string_to_file' do
     expect(described_class.output_string_to_file('a', '/tmp/out.out')).not_to be_nil
   end
-
-  it 'read_array_from_file' do
-    described_class.output_string_to_file("a\na", '/tmp/read_array.file')
-    expect(described_class.read_array_from_file('/tmp/read_array.file')).to eq(%w[a a])
-  end
 end
