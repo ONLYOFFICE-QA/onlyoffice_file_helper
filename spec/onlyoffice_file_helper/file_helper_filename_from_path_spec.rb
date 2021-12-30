de6c403849c5d24597c986f38b4a52dce5333933
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-12-30 16:44:47 +0300

    Remove deprecated `OnlyofficeFileHelper.get_filename` (#184)
    * Remove deprecated `OnlyofficeFileHelper.get_filename`
    
    * Fix more
    
    * Bring back tests
    
    * Fix more

--- spec/onlyoffice_file_helper/file_helper_filename_from_path_spec.rb
+++ spec/onlyoffice_file_helper/file_helper_filename_from_path_spec.rb
@@ -2,18 +2,18 @@
 
 require 'spec_helper'
 
-RSpec.describe OnlyofficeFileHelper::FileHelper, '.get_filename' do
+RSpec.describe OnlyofficeFileHelper::FileHelper, '.filename_from_path' do
   it 'get_filename is correct for correct data' do
-    expect(described_class.get_filename('/usr/share/fonts')).to eq('fonts')
+    expect(described_class.filename_from_path('/usr/share/fonts')).to eq('fonts')
   end
 
   it 'get_filename is correct with keep extensions' do
     file = described_class.create_file_with_content(file_path: '/tmp/onlyoffice_filehelper_test.jpg')
-    expect(described_class.get_filename(file, true)).to eq('onlyoffice_filehelper_test.jpg')
+    expect(described_class.filename_from_path(file, keep_extension: true)).to eq('onlyoffice_filehelper_test.jpg')
   end
 
   it 'get_filename is correct without keep extensions' do
     file = described_class.create_file_with_content(file_path: '/tmp/onlyoffice_filehelper_test.jpg')
-    expect(described_class.get_filename(file, false)).to eq('onlyoffice_filehelper_test')
+    expect(described_class.filename_from_path(file, keep_extension: false)).to eq('onlyoffice_filehelper_test')
   end
 end
