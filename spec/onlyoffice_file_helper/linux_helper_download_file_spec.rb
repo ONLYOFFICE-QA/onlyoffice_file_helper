cbe44c954786d3e428e6aa173e9217fd68b3e1d1
OnlyOffice Testing Robot <teamlab.ruby@gmail.com>
2023-05-26 11:21:30 +0300

    Fix csv file url in specs (#327)

--- spec/onlyoffice_file_helper/linux_helper_download_file_spec.rb
+++ spec/onlyoffice_file_helper/linux_helper_download_file_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 
 RSpec.describe OnlyofficeFileHelper::LinuxHelper, '.download_file' do
   it 'Download file is not zero after download' do
-    file_url = 'https://file-examples.com/wp-content/uploads/2017/02/file_example_CSV_5000.csv'
+    file_url = 'https://github.com/ONLYOFFICE-QA/onlyoffice_file_helper/files/11573012/file_example_CSV_5000.csv'
     downloaded = Tempfile.new('onlyoffice_file_helper')
     described_class.download_file(file_url, downloaded.path)
     expect(File.size(downloaded.path)).to be > 1_000
